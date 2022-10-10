1ea69040cf14fc9b7dc105c462acd7c89d9f0168
sky9023 <25349023.qq@gmail.com>
2022-10-11 00:18:33 +0800

    [UPDATE] refactor of saving images; rearrange the code

--- 1/HW1-1.py
+++ 1/HW1-1.py
@@ -49,12 +49,6 @@ def show_multiple_image(images, titles):
     fig.show()
 
 
-def show_then_write(images, titles):
-    show_multiple_image(images, titles)
-    for image, title in zip(images, titles):
-        cv2.imwrite(f'output/{title}.jpg', normalize(image, 0, 255))
-
-
 def gradient_field(image, threshold_ratio=0.0):
     sobel = (np.array([[1., 2, 1], [0, 0, 0], [-1, -2, -1]]) / 8,
              np.array([[-1., 0, 1], [-2, 0, 2], [-1, 0, 1]]) / 8)  # (hy, hx)
@@ -81,12 +75,6 @@ def direction_in_hue(magnitude, direction):
     hsv[..., 2] = normalize(magnitude.mean(axis=-1), 0, 255)
     rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
     return rgb
-
-
-def show_gradient(magnitude, direction, name):
-    dire_rgb = direction_in_hue(magnitude, direction)
-    show_then_write([magnitude, dire_rgb],
-                    [f'{name}_grad_magnitude', f'{name}_grad_direction'])
 
 
 def summation_kernel(size):
@@ -141,67 +129,74 @@ def non_maximal_suppression(raw_corner: np.ndarray, w_radius=5, threshold=1e-2):
 
 
 def show_corner(image, corner, size, name):
-    plt.figure(figsize=(10, 10))
-    imshow(image, f'corner_detection_{size}')
-    plt.scatter(corner.nonzero()[1], corner.nonzero()[0], c='r')
-    plt.savefig(f'output/{name}_corner_detection_{size}.jpg')
-    plt.show()
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    imshow(image, f'{name}_corner_detection_{size}')
+    ax.scatter(corner.nonzero()[1], corner.nonzero()[0], c='r')
+    fig.canvas.draw()
+    data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    data = data[..., ::-1]
+    fig.show()
+
+    return data
+
+
+def corner_detection(original_image, name):
+    img_to_save = {}
+
+    # Step 1: gaussian smooth
+    blurrer5x5 = gaussian_blurrer(5, 5)
+    blurrer10x10 = gaussian_blurrer(10, 5)
+
+    blurred_5x5 = blurrer5x5(original_image)
+    blurred_10x10 = blurrer10x10(original_image)
+    show_multiple_image([blurred_5x5, blurred_10x10],
+                        [f'{name}_blur5x5', f'{name}_blur10x10'])
+    img_to_save.update({f'{name}_blur5x5': blurred_5x5,
+                        f'{name}_blur10x10': blurred_10x10})
+
+    blurred_images = {f'5x5': blurred_5x5,
+                      f'10x10': blurred_10x10}
+
+    for suffix, bimg in blurred_images.items():
+        # Step 2
+        grad, mag, dire = gradient_field(bimg, 0.1)
+        dire_rgb = direction_in_hue(mag, dire)
+        show_multiple_image([mag, dire_rgb],
+                            [f'{name}_{suffix}_grad_magnitude', f'{name}_{suffix}_grad_direction'])
+        img_to_save.update({f'{name}_{suffix}_grad_magnitude': mag,
+                            f'{name}_{suffix}_grad_direction': dire_rgb})
+
+        if suffix == '10x10':
+            # Step 3
+            Hs = structure_tensor(grad)
+
+            windows = {'3x3': summation_kernel(3), '5x5': summation_kernel(5)}
+
+            for suffix, window in windows.items():
+                eigen_minus, resp = calc_eigen_minus(window, *Hs)
+                eigen_minus = normalize(eigen_minus, 0, 1)
+
+                imshow(eigen_minus, f'{name}_eigen_{suffix}')
+                img_to_save.update({f'{name}_eigen_{suffix}': eigen_minus})
+
+                # Step 4
+                corners = non_maximal_suppression(eigen_minus, 25, 0.15)
+                result = show_corner(original_image, corners, suffix, name)
+                img_to_save.update({f'{name}_corner_detection_{suffix}': result})
+
+    return img_to_save
 
 
 if __name__ == '__main__':
+    images_to_save = {}
+
     chessboard = cv2.imread('chessboard-hw1.jpg')
     notredame = cv2.imread('1a_notredame.jpg')
     show_multiple_image([chessboard, notredame], ['original chessboard', 'original_notredame'])
 
-    # Step 1
-    blurrer5x5 = gaussian_blurrer(5, 5)
-    blurrer10x10 = gaussian_blurrer(10, 5)
+    images_to_save.update(corner_detection(chessboard, 'chessboard'))
+    images_to_save.update(corner_detection(notredame, 'notredame'))
 
-    chessboard_5x5 = blurrer5x5(chessboard)
-    chessboard_10x10 = blurrer10x10(chessboard)
-    show_then_write([chessboard_5x5, chessboard_10x10],
-                    ['chessboard_blur5x5', 'chessboard_blur10x10'])
-
-    notredame_5x5 = blurrer5x5(notredame)
-    notredame_10x10 = blurrer10x10(notredame)
-    show_then_write([notredame_5x5, notredame_10x10],
-                    ['notredame_blur5x5', 'notredame_blur10x10'])
-
-    plt.show()
-
-    # Step 2
-    images = {'chessboard_5x5': chessboard_5x5,
-              'chessboard_10x10': chessboard_10x10,
-              'notredame_5x5': notredame_5x5,
-              'notredame_10x10': notredame_10x10}
-
-    for name, img in images.items():
-        grad, mag, dire = gradient_field(img, 0.1)
-        show_gradient(mag, dire, name)
-
-    images_10x10 = {'chessboard': (chessboard_10x10, chessboard),
-                    'notredame': (notredame_10x10, notredame)}
-    window3x3 = summation_kernel(3)
-    window5x5 = summation_kernel(5)
-
-    for name, (img, oimg) in images_10x10.items():
-        # Step 3
-        grad, mag, dire = gradient_field(img, 0.1)
-
-        Hs = structure_tensor(grad)
-
-        eigen_minus3x3, resp3x3 = calc_eigen_minus(window3x3, *Hs)
-        eigen_minus3x3 = normalize(eigen_minus3x3, 0, 1)
-
-        eigen_minus5x5, resp5x5 = calc_eigen_minus(window5x5, *Hs)
-        eigen_minus5x5 = normalize(eigen_minus5x5, 0, 1)
-
-        show_then_write([eigen_minus3x3, eigen_minus5x5], [f'{name}_eigen_3x3', f'{name}_eigen_5x5'])
-
-        # Step 4
-        corners3x3 = non_maximal_suppression(eigen_minus3x3, 25, 0.15)
-        corners5x5 = non_maximal_suppression(eigen_minus5x5, 25, 0.15)
-        show_corner(oimg, corners3x3, '3x3', name)
-        show_corner(oimg, corners5x5, '5x5', name)
-
-
+    for filename, img in images_to_save.items():
+        cv2.imwrite(f'output/normal/{filename}.jpg', normalize(img, 0, 255))
