4ced3f6fcae7150494ce5544791ba08c35542796
sky9023 <25349023.qq@gmail.com>
2022-10-10 20:39:05 +0800

    [UPDATE] migrate notebook to py, part 1-3

--- 1/HW1-1.py
+++ 1/HW1-1.py
@@ -90,6 +90,31 @@ def show_gradient(magnitude, direction, name):
                     [f'{name}_grad_magnitude', f'{name}_grad_direction'])
 
 
+def summation_kernel(size):
+    k = np.ones((size, size))
+    return functools.partial(apply_kernel, k)
+
+
+def structure_tensor(gradient):
+    Ixx = gradient[1] ** 2
+    Ixy = gradient[0] * gradient[1]
+    Iyy = gradient[0] ** 2
+
+    return Ixx, Ixy, Iyy
+
+
+def calc_eigen_minus(window, Ixx, Ixy, Iyy):
+    Ixx = window(Ixx)
+    Ixy = window(Ixy)
+    Iyy = window(Iyy)
+
+    det = Ixx * Iyy - Ixy * Ixy
+    trace = Ixx + Iyy + 1e-5
+    eigen_minus = det / trace
+    response = det - 0.04 * (trace ** 2)
+    return eigen_minus, response
+
+
 if __name__ == '__main__':
     img = cv2.imread('chessboard-hw1.jpg')
     img2 = cv2.imread('1a_notredame.jpg')
@@ -121,3 +146,20 @@ if __name__ == '__main__':
         grad, mag, dire = gradient_field(img, 0.1)
         show_gradient(mag, dire, name)
 
+    images_10x10 = {k[:-5]: v for k, v in images.items() if '10x10' in k}
+    window3x3 = summation_kernel(3)
+    window5x5 = summation_kernel(5)
+
+    for name, img in images_10x10.items():
+        # Step 3
+        grad, mag, dire = gradient_field(img, 0.1)
+
+        Hs = structure_tensor(grad)
+
+        eigen_minus3x3, resp3x3 = calc_eigen_minus(window3x3, *Hs)
+        eigen_minus3x3 = normalize(eigen_minus3x3, 0, 1)
+
+        eigen_minus5x5, resp5x5 = calc_eigen_minus(window5x5, *Hs)
+        eigen_minus5x5 = normalize(eigen_minus5x5, 0, 1)
+
+        show_then_write([eigen_minus3x3, eigen_minus5x5], [f'{name}_eigen_3x3', f'{name}_eigen_5x5'])
